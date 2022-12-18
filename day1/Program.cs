6b6807e6862539a73a5821e445128daa894f5606
Gabriele Mastrapasqua <gabriele.mastrapasqua@gmail.com>
2022-12-18 10:50:13 +0100

    chore - refactor

--- day1/Program.cs
+++ day1/Program.cs
@@ -3,37 +3,56 @@
 using System;
 using System.Text.RegularExpressions;
 
-string input = "input.txt";
-var enviroment = System.Environment.CurrentDirectory;
-string ?projectDirectory = Directory.GetParent(enviroment)?.Parent?.FullName;
-string text = null;
-try {
-    text = System.IO.File.ReadAllText($"{projectDirectory}/../{input}");
-}catch(FileNotFoundException ex) {
-    // when running from cli dotnet run
-    text = System.IO.File.ReadAllText($"./{input}");
+string ReadFile(string input = "input.txt")
+{
+    
+    var enviroment = System.Environment.CurrentDirectory;
+    string? projectDirectory = Directory.GetParent(enviroment)?.Parent?.FullName;
+    string text = null;
+    try
+    {
+        text = System.IO.File.ReadAllText($"{projectDirectory}/../{input}");
+    }
+    catch (FileNotFoundException ex)
+    {
+        // when running from cli dotnet run
+        text = System.IO.File.ReadAllText($"./{input}");
+    }
+    return text;
 }
 
 
-// Split for each empty lines is an elves
-Regex rx = new Regex(@"^$[\r\n]+", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
-var elves = rx.Split(text);
-//Console.WriteLine(elves.Count());
+IEnumerable<int>[] SplitElves(string text)
+{
+    // 1 - Split empty lines, so we can know calories for each elf
+    Regex rx = new Regex(@"^$[\r\n]+", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
+    var elves = rx.Split(text);
 
-// map lines for each elves, cast to number, to array
-var elvesCalories = elves
-    .Select(str => str.Split("\n"))
-    .Select(lines => lines.ToArray()
-        .Where(item => item != "")
-        .Select(line => int.Parse(line)))
-    .ToArray();
+    // 2 - Map lines for each elves, cast to number, to array
+    var elvesCalories = elves
+        .Select(str => str.Split("\n"))
+        .Select(lines => lines.ToArray()
+            .Where(item => item != "")
+            .Select(line => int.Parse(line)))
+        .ToArray();
+        
+    return elvesCalories;
+}
 
-// then sum foreach elves
+
+
+// Read from input txt calories lines 
+var text = ReadFile("input.txt");
+
+// Split for each empty lines is an elves then map the correct calories for each elf and cast to number
+var elvesCalories = SplitElves(text);
+
+// then sum foreach elves and find the max value
 var max = 0;
 foreach (var calories in elvesCalories)
 {
     var sum = calories.Sum();
-    if(sum > max)
+    if (sum > max)
     {
         max = sum;
     }
