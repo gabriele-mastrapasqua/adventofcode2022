7a97695ff38333defd245cd68bc5c051706b87f0
Gabriele Mastrapasqua <gabriele.mastrapasqua@gmail.com>
2022-12-18 10:36:02 +0100

    day 1

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
 
+using System;
+using System.Text.RegularExpressions;
+
+string input = "input.txt";
+var enviroment = System.Environment.CurrentDirectory;
+string ?projectDirectory = Directory.GetParent(enviroment)?.Parent?.FullName;
+string text = System.IO.File.ReadAllText($"{projectDirectory}/../{input}");
+
+
+// Split for each empty lines is an elves
+Regex rx = new Regex(@"^$[\r\n]+", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
+var elves = rx.Split(text);
+Console.WriteLine(elves.Count());
+
+// map lines for each elves, cast to number, to array
+var elvesCalories = elves
+    .Select(str => str.Split("\n"))
+    .Select(lines => lines.ToArray()
+        .Where(item => item != "")
+        .Select(line => int.Parse(line)))
+    .ToArray();
+
+// then sum foreach elves
+var max = 0;
+foreach (var calories in elvesCalories)
+{
+    var sum = calories.Sum();
+    if(sum > max)
+    {
+        max = sum;
+    }
+}
+Console.WriteLine($"Max calories for all elves: {max}");
