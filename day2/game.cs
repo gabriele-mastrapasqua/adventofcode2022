f96afa6aacd962484eb4a7af00a4666e40d99964
Gabriele Mastrapasqua <gabriele.mastrapasqua@gmail.com>
2022-12-20 13:59:12 +0100

    feat - refactor some code

---
+++ day2/game.cs
@@ -0,0 +1,21 @@
+﻿
+static class Utils
+{
+    static public string ReadFile(string input = "input.txt")
+    {
+
+        var enviroment = Environment.CurrentDirectory;
+        string? projectDirectory = Directory.GetParent(enviroment)?.Parent?.FullName;
+        string text = null;
+        try
+        {
+            text = File.ReadAllText($"{projectDirectory}/../{input}");
+        }
+        catch (FileNotFoundException ex)
+        {
+            // when running from cli dotnet run
+            text = File.ReadAllText($"./{input}");
+        }
+        return text;
+    }
+}
